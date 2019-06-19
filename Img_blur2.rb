cf1c37d984bae9c634544e677ca26556febbe5ce
Aichurek Zhalilova <anadan20182019@gmail.com>
2019-06-19 21:49:28 +0000

    Blur2

--- Img_blur2.rb
+++ Img_blur2.rb
@@ -2,7 +2,7 @@
 
 $k
 def setImg
-    $k=[[0,0,0,1],[0,1,0,0],[1,0,1,0,1],[0,0,0,0]]
+    $k=[[1,0,0,0],[0,0,0,0],[0,0,1,0],[0,0,0,0]]
   end 
   def show
   for i in 0..$k.length-1
@@ -14,13 +14,16 @@ def setImg
   end
 
 def convert_2
-   for i in 0..$k.length-1
-     for j in 0..$k.length-1
+   for i in 0..3
+     for j in 0..3
       if ($k[i][j]==1) 
-        $k[i-1][j]=2 unless i<=0 and $k[i-1][j]==0
-        $k[i][j-1]=2 unless j<=0 and $k[i][j-1]==0
-        $k[i+1][j]=2 unless i>=$k.length-1 and $k[i+1][j]==0
-        $k[i][j+1]=2 unless j>=$k.length-1 and $k[i][j+1]==0
+        
+        $k[i-1][j]=2 if (i-1)>=0 and (i-1)<=$k.length-1 and  (j)>=0 and (j)<=$k.length-1  and $k[i-1][j]==0
+        $k[i][j-1]=2 if (i)>=0 and (i)<=$k.length-1 and  (j-1)>=0 and (j-1)<=$k.length-1  and $k[i][j-1]==0
+        $k[i+1][j]=2 if (i+1)>=0 and (i+1)<=$k.length-1 and  (j)>=0 and (j)<=$k.length-1  and $k[i+1][j]==0    
+        $k[i][j+1]=2 if (i)>=0 and (i)<=$k.length-1 and  (j+1)>=0 and (j+1)<=$k.length-1  and $k[i][j+1]==0
+      
+
       end
      end
      puts"\n"
