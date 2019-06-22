9869f4ed9ee258bb52f0fe8389388bdf3bb5a430
Aichurek Zhalilova <anadan20182019@gmail.com>
2019-06-22 01:27:19 +0000

    V

--- db/migrate/20190609011732_alter_places_add_lat_and_lng.rb
+++ db/migrate/20190609011732_alter_places_add_lat_and_lng.rb
@@ -1,8 +1,6 @@
 class AlterPlacesAddLatAndLng < ActiveRecord::Migration[5.2]
   def change
     add_column :places, :latitude, :float
-    geocoded_by :address
-    after_validation :geocode
     add_column :places, :longitude, :float
   end
 end
